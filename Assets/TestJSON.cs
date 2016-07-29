1bc34c68c6cdc6e4d9e9c46ac929574864894c7d
laol <litvinovandrey@list.ru>
2016-07-29 13:00:24 +0500

    start

---
+++ Assets/TestJSON.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestJSON : MonoBehaviour {
+
+    public bool checkMe;
+  
+    public int[,] level;
+  
+    List<string> test = new List<string>();
+
+    string json;
+    
+    int i = 1;
+
+	void Update () {
+        if (checkMe)
+        {
+            level = new int[i, i];
+            for (int j = 0; j < i; j++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    level[j, k] = i;
+                }
+
+            }
+            ++i;
+            checkMe = false;
+            test.Add(JsonUtility.ToJson(level));
+        }
+    }
+}

60c43425068ad04d0d69002fefcea2d1efb5a295
laol <litvinovandrey@list.ru>
2016-07-29 17:24:06 +0500

    refactoring nuitrack part

--- Assets/Nuitrack/WorkWithSensor/NuitrackManager.cs
+++ Assets/Nuitrack/WorkWithSensor/NuitrackManager.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class NuitrackManager : MonoBehaviour {
 
     [SerializeField]
-    bool isHaveSkeletonTracker;
-    [SerializeField]
-    bool isHaveHandTracker;
-    [SerializeField]
-    bool isHaveUserTracker;
+    bool isHaveSkeletonTracker, 
+         isHaveHandTracker,
+         isHaveUserTracker,
+         isHaveDepthSensor;
 
-	void Start ()
+	void Awake()
     {
-	    
-	}
+        if (isHaveSkeletonTracker) gameObject.AddComponent<SkeletonTracker>();
+        if (isHaveHandTracker) gameObject.AddComponent<HandTracker>();
+        if (isHaveUserTracker) gameObject.AddComponent<UserTracker>();
+        if (isHaveDepthSensor) gameObject.AddComponent<DepthSensor>();
+    }
 	
 
 	void Update ()

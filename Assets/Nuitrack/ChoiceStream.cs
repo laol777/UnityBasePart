a81d83e076c174902ac53164a8e6f72845625f5b
laol <litvinovandrey@list.ru>
2016-08-11 02:00:20 +0500

    dynamic choice between stream data: sensor or JSON

--- Assets/Nuitrack/ChoiceStream.cs
+++ Assets/Nuitrack/ChoiceStream.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System;
 
 public class ChoiceStream : MonoBehaviour {
 
@@ -16,25 +16,104 @@ public class ChoiceStream : MonoBehaviour {
         userTracker = GameObject.FindObjectOfType<UserTracker>();
     }
 
+    int[,] ConvertNuitrackObjToIntArray<T>(T array) where T : nuitrack.Frame<ushort>
+    {
+
+        nuitrack.UserFrame tt = new nuitrack.UserFrame();
+
+        int YRes = DepthSensor.DepthFrame.Rows;
+        int XRes = DepthSensor.DepthFrame.Cols;
+        int[,] resault = new int[YRes, XRes];
+        for (int i = 0; i < YRes; ++i)
+            for (int j = 0; j < XRes; ++j)
+            {
+                //resault[i, j] = (int)Convert.ChangeType(array[i, j], typeof(int));
+                //resault[i, j] = array[i * XRes + j];
+                resault[i, j] = array[i * XRes + j];
+            }
+        return resault;
+    }
+
     public int[,] GetDepthFrame()
     {
-        //if (Application.platform == RuntimePlatform.WindowsEditor)
+        if (Application.platform == RuntimePlatform.WindowsEditor)
         {
             return nuitrackManagerEmulation.DepthFrame;
         }
-        //else
-        //{
-        //    return DepthSensor.DepthFrame;
-        //}
+        else
+        {
+            if (DepthSensor.DepthFrame != null)
+            {
+                return ConvertNuitrackObjToIntArray<nuitrack.DepthFrame>(DepthSensor.DepthFrame);
+            }
+            else
+            {
+                return null; 
+            }
+        }
     }
 
     public int[,] GetUserFrame()
     {
-        return nuitrackManagerEmulation.UserFrame;
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            return nuitrackManagerEmulation.UserFrame;
+        }
+        else
+        {
+            if (UserTracker.UserFrame != null)
+            {
+                return ConvertNuitrackObjToIntArray<nuitrack.UserFrame>(UserTracker.UserFrame);
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 
-    public int Frame { get { return nuitrackManagerEmulation.Frame; } }
+    public int Frame
+    {
+        get
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                return nuitrackManagerEmulation.Frame;
+            }
+            else
+            {
+                return DepthSensor.frame;
+            }
+        }
+        
+    }
 
-    public int XRes { get { return nuitrackManagerEmulation.XRes; } }
-    public int YRes { get { return nuitrackManagerEmulation.YRes; } }
+    public int XRes
+    {
+        get
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                return nuitrackManagerEmulation.XRes;
+            }
+            else
+            {
+                return DepthSensor.DepthFrame.Cols;
+            }
+        }
+    }
+    public int YRes
+    {
+        get
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                return nuitrackManagerEmulation.YRes;
+            }
+            else
+            {
+                return DepthSensor.DepthFrame.Rows;
+            }
+        }
+    }
 }

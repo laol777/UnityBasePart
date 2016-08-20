ff5ee1cef7181c939deea601203e521713cb2564
laol <litvinovandrey@list.ru>
2016-08-20 20:36:56 +0500

    start init network player

--- Assets/Scripts/PlayerBeahaviour.cs
+++ Assets/Scripts/PlayerBeahaviour.cs
@@ -15,7 +15,20 @@ public class PlayerBeahaviour : NetworkBehaviour
     ChoiceStream choiceStream;
 
     SensorRotation sensorRotation;
-    // Use this for initialization
+
+
+    public Vector3 tmpPos;
+    public int numberUser = 1;
+
+    [SerializeField]
+    Transform head;
+    [SerializeField]
+    Transform rightWrist;
+    [SerializeField]
+    Transform rightElbow;
+    [SerializeField]
+    Transform rotationPivot;
+
     void Start()
     {
         StartCoroutine(RedactPlayerPrefabName()); // hasAuthorithy change value after 1 frame
@@ -44,30 +57,36 @@ public class PlayerBeahaviour : NetworkBehaviour
         if ((hasAuthority && isServer) || (!hasAuthority && !isServer))
         {
             gameObject.name = "hostPlayer";
-            offset = new Vector3(0f, 0f, 3f);
+            offset = new Vector3(0f, 0f, 5f);
             startRotation = Quaternion.Euler(0f, 0f, 0f);
             sensorRotation.SetBaseRotation(startRotation);
+            numberUser = 1;
+            if (isServer)
+            {
+                rotationPivot.rotation *= startRotation;
+                transform.position += offset;
+                sensorRotation.SetBaseRotation(Quaternion.Euler(0f, 180f, 0f));
+            }
         }
         else
         {
             gameObject.name = "clientPlayer";
-            offset = new Vector3(0f, 0f, -3f);
-            startRotation = Quaternion.Euler(0f, 0f, 0f);
+            offset = new Vector3(0f, 0f, -5f);
+            startRotation = Quaternion.Euler(0f, 180f, 0f);
+            sensorRotation.SetBaseRotation(startRotation);
+            numberUser = 2;
+            rotationPivot.rotation *= startRotation;
+            transform.position += offset;
         }
 
-        transform.position += offset;
+        if(!hasAuthority)
+        {
+            camera.gameObject.SetActive(false);
+        }
     }
 
 
-    public Vector3 tmpPos;
-    public int numberUser = 1;
 
-    [SerializeField]
-    Transform head;
-    [SerializeField]
-    Transform rightWrist;
-    [SerializeField]
-    Transform rightElbow;
 
     void Update()
     {

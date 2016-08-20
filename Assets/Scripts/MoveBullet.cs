4456aa37b1a450a2a8090c97d26d9fde31754d2c
laol <litvinovandrey@list.ru>
2016-08-20 22:28:58 +0500

    developint shooting behaviour

---
+++ Assets/Scripts/MoveBullet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBullet : MonoBehaviour {
+
+    public float velocity = 1f;
+    public Vector3 vector;
+	
+	void Update () {
+        transform.position += vector * velocity * Time.deltaTime;
+	}
+}
--- Assets/Scripts/PlayerBeahaviour.cs
+++ Assets/Scripts/PlayerBeahaviour.cs
@@ -42,18 +42,6 @@ public class PlayerBeahaviour : NetworkBehaviour
     IEnumerator RedactPlayerPrefabName()
     {
         yield return new WaitForSeconds(0.1f);
-        //if ((hasAuthority && isServer) || (!hasAuthority && !isServer))
-        //{
-        //    gameObject.name = "hostPlayer";
-        //    offset = new Vector3(0f, 0f, 3f);
-        //    startRotation = Quaternion.Euler(0f, 0f, 0f);
-        //}
-        //else
-        //{
-        //    gameObject.name = "clientPlayer";
-        //    offset = new Vector3(0f, 0f, -3f);
-        //    startRotation = Quaternion.Euler(0f, 0f, 0f);
-        //}
         if ((hasAuthority && isServer) || (!hasAuthority && !isServer))
         {
             gameObject.name = "hostPlayer";
@@ -79,14 +67,32 @@ public class PlayerBeahaviour : NetworkBehaviour
             transform.position += offset;
         }
 
-        if(!hasAuthority)
+        if(!hasAuthority) //disable, when is starting as remote player on current device
         {
             camera.gameObject.SetActive(false);
+            cursor.SetActive(false);
         }
     }
 
+    public GameObject bullet;
+    public GameObject cameraAim;
+    public GameObject cursor;
+    public GameObject quadCamera;
+    public GameObject quadCursor;
+
+    bool scalePositionCursor = false;
+    bool prevScalePositionCursor = false;
+
+    public Vector3 offs;
 
 
+    bool isStartFail;
+    Vector3 tmpF;
+    float timeFail = 5f;
+
+    bool isHaveAbilityFail;
+
+    float time = 0f;
 
     void Update()
     {
@@ -104,5 +110,82 @@ public class PlayerBeahaviour : NetworkBehaviour
             rightElbow.localPosition = tmpPos;
 
         }
+
+        Vector3 vectorShoot = Vector3.Normalize(rightWrist.position - rightElbow.position);
+        cursor.transform.localPosition = new Vector3(vectorShoot.x * 2f, vectorShoot.y * 2f, -10f);
+
+        time += Time.deltaTime;
+        if (time > 1f)
+        {
+            time = 0f;
+            GameObject tmp = (GameObject)Instantiate(bullet, rightWrist.position, Quaternion.identity);
+            tmp.GetComponent<MoveBullet>().vector = vectorShoot;
+            tmp.GetComponent<MoveBullet>().velocity = 3f;
+            Destroy(tmp, 10f);
+        }
+
+        /*
+        if (hasAuthority)
+        {
+            #region myPart
+
+
+
+
+            if (cursor != null)
+            {
+                Vector3 vectorShoot = Vector3.Normalize(cameraAim.transform.position - rightWrist.position);
+                float angle = Vector3.Angle(vectorShoot, new Vector3(0f, 0f, 1f));
+
+                if (angle < 30f)
+                {
+                    scalePositionCursor = true;
+                }
+                else if ((angle < 60f) && (prevScalePositionCursor))
+                {
+                    scalePositionCursor = true;
+                }
+                else
+                {
+                    scalePositionCursor = false;
+                }
+
+                if (scalePositionCursor)
+                {
+                    cursor.transform.position = new Vector3(vectorShoot.x * 2f, vectorShoot.y * 2f, 11.5f);
+                }
+
+                cursor.SetActive(scalePositionCursor);
+
+                if (quadCamera != null)
+                {
+                    quadCamera.SetActive(scalePositionCursor);
+                }
+
+                Vector3 tmp = new Vector3(vectorShoot.x * 0.075f, vectorShoot.y * 0.075f, 0f);
+
+                if (quadCursor != null)
+                {
+                    quadCursor.transform.localPosition = tmp;
+                }
+                prevScalePositionCursor = scalePositionCursor;
+
+                float xOffset = (vectorShoot.x + 1f) / 2f;
+                float yOffset = (vectorShoot.y + 1f) / 2f;
+
+                tmp = Vector3.Normalize(cursor.transform.position - rightWrist.position) * 90f;
+                rightWrist.rotation = Quaternion.Euler(tmp.x, tmp.y, tmp.z);
+
+                offs.y = -30f;
+
+                rightWrist.localRotation *= Quaternion.Euler(offs);
+
+            }
+                      #endregion
+                       }
+            */
+
+
+
     }
 }
